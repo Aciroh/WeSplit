16a2843ca52559ce6724603ba002ab7ef52c3e39
Aciroh <horatiubolovan@gmail.com>
2022-08-25 14:41:07 +0300

    Add functionality to the app: Picker for Tips, Picker for # of people, Text to output total per person and total including the check amount and the tips.

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -3,16 +3,27 @@ import SwiftUI
 struct ContentView: View {
 
     @State private var checkAmount = 0.0
+    @FocusState private var amountIsFocused: Bool
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
-    let tipPercentages = [10, 15, 20, 25, 0]
+    let tipPercentages = Array(0..<101)
+    let format = FloatingPointFormatStyle<Double>.Currency.currency(code: Locale.current.currencyCode ?? "EUR")
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        return amountPerPerson
+    }
     
     var body: some View {
         NavigationView{
             Form{
                 Section{
-                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currencyCode ?? "EUR"))
+                    TextField("Amount", value: $checkAmount, format: format)
                         .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
                     Picker("Number of people", selection: $numberOfPeople) {
                         ForEach(2..<100) {
                             Text("\($0) people")
@@ -25,12 +36,29 @@ struct ContentView: View {
                             Text($0, format: .percent)
                         }
                     }
+                } header: {
+                    Text("Tip percentage:")
                 }
                 Section {
-                    Text("Check amount: \(checkAmount)")
+                    Text(totalPerPerson, format: format)
+                } header: {
+                    Text("Ammount per person:")
+                }
+                Section {
+                    Text(checkAmount + (checkAmount * (Double(tipPercentage) / 100)), format: format)
+                } header: {
+                    Text("Check and tips total:")
                 }
             }
             .navigationTitle("WeSplit")
+            .toolbar {
+                ToolbarItemGroup(placement: .keyboard) {
+                    Spacer()
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
         }
         
     }
